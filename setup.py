23da12f271c9a4803f051e1141d1fd470c3513c0
RoboticsYY <yu.yan@intel.com>
2019-10-18 17:21:45 +0800

    Migrate to ROS2 package

--- setup.py
+++ setup.py
@@ -1,11 +1,34 @@
-#!/usr/bin/env python
+from setuptools import find_packages
+from setuptools import setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'handeye'
 
-d = generate_distutils_setup(
-    packages=['handeye'],
-    package_dir={'': 'src'}
+setup(
+    name=package_name,
+    version='0.1.1',
+    packages=find_packages('src', exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Francisco Suarez-Ruiz',
+    author_email='fsuarez6@gmail.com',
+    maintainer='Francisco Suarez-Ruiz',
+    maintainer_email='fsuarez6@gmail.com',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD 3-Clause License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=(
+        'The handeye calibration algorithm package.'
+    ),
+    license='BSD 3-Clause License',
+    tests_require=['pytest'],
+    package_dir={'':'src'}, 
 )
-
-setup(**d)
